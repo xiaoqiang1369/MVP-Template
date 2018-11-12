d36879a092bc89e4c606df00dbbe64b7e5786388
xiaoqiang1369 <578570174@qq.com>
2018-11-12 14:16:54 +0800

    Update BasePresenter.kt

--- base/kotlin/BasePresenter.kt
+++ base/kotlin/BasePresenter.kt
@@ -1,25 +1,10 @@
 package xiaoqiang.com.jetpacktest.base.kotlin
 
-import androidx.lifecycle.LifecycleOwner
-import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
-
-
 /**
  *   description: base presenter for MVP
  *   created by crx on 2018/11/7 15:59
  */
 interface BasePresenter {
-
-
-
+    
     fun start()
-
-    fun <T> getProvider(mView: T) : AndroidLifecycleScopeProvider {
-        if(mView is LifecycleOwner){
-            return AndroidLifecycleScopeProvider.from(mView.lifecycle)
-        }else{
-            throw IllegalArgumentException("The View is not a LifecycleOwner!")
-        }
-
-    }
 }
